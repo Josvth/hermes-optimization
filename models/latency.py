c3f09f59bbd687d87280efd26bc254432eba74f3
Jos van 't Hof <josvth@gmail.com>
2021-03-28 15:25:11 -0700

    Commit before changing lists to something else.

--- models/latency.py
+++ models/latency.py
@@ -28,3 +28,15 @@ def compute_latency_passes(tof_s_list, margin_dB_list, t_sim_s):
     latency = np.sum(0.5 * (diff_s)**2)
 
     return latency / t_sim_s
+
+@njit(parallel=True)
+def compute_max_latency_passes(tof_s_list, margin_dB_list, t_sim_s):
+
+    b_s_array, e_s_array = actual_begin_end(tof_s_list, margin_dB_list)
+
+    e_s_array = np.append([0.], e_s_array)
+    b_s_array = np.append(b_s_array, t_sim_s)
+
+    diff_s = b_s_array - e_s_array
+
+    return np.max(diff_s)
